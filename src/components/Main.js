4dad1a1659cc1cefc29b90ebe968b36e6a04c64b
Dan Esparza <esparza.dan@gmail.com>
2017-10-11 14:20:22 -0400

    Add rest of query templates

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,16 +23,13 @@ class Main extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false,
       queryText: ""    
     };
-
-    this._onQueryChange = this._onQueryChange.bind(this);
   }
 
-  toggle() {
+  toggle = () => {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
     });
@@ -50,7 +47,7 @@ class Main extends Component {
                   <form onSubmit={this._onQuerySubmit}>
                     <div id="influxQuery" className="input-group">
                       <span className="input-group-addon" id="basic-addon1">Query:</span>
-                      <input autoFocus type="text" id="txtQuery" value={this.state.queryText} onChange={this._onQueryChange} className="form-control" aria-label="Influx query" aria-describedby="basic-addon1"/>
+                      <input autoFocus ref={(input) => { this.queryInput = input; }} type="text" value={this.state.queryText} onChange={this._onQueryChange} className="form-control" aria-label="Influx query" aria-describedby="basic-addon1"/>
                     </div>
                   </form>
                 </div>
@@ -71,21 +68,21 @@ class Main extends Component {
                         <DropdownItem onClick={this.showTagKeysText}>Show tag keys</DropdownItem>
                         <DropdownItem onClick={this.showTagValuesText}>Show tag values</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem>Show retention policies</DropdownItem>
-                        <DropdownItem>Create retention policy</DropdownItem>
-                        <DropdownItem>Drop retention policy</DropdownItem>
+                        <DropdownItem onClick={this.showRetentionPolicyText}>Show retention policies</DropdownItem>
+                        <DropdownItem onClick={this.createRetentionPolicyText}>Create retention policy</DropdownItem>
+                        <DropdownItem onClick={this.dropRetentionPolicyText}>Drop retention policy</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem>Show continuous queries</DropdownItem>
-                        <DropdownItem>Create continuous query</DropdownItem>
-                        <DropdownItem>Drop continuous query</DropdownItem>
+                        <DropdownItem onClick={this.showContinuousQueryText}>Show continuous queries</DropdownItem>
+                        <DropdownItem onClick={this.createContinuousQueryText}>Create continuous query</DropdownItem>
+                        <DropdownItem onClick={this.dropContinuousQueryText}>Drop continuous query</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem>Show users</DropdownItem>
-                        <DropdownItem>Create user</DropdownItem>
-                        <DropdownItem>Create admin user</DropdownItem>
-                        <DropdownItem>Drop user</DropdownItem>
+                        <DropdownItem onClick={this.showUserText}>Show users</DropdownItem>
+                        <DropdownItem onClick={this.createUserText}>Create user</DropdownItem>
+                        <DropdownItem onClick={this.createAdminUserText}>Create admin user</DropdownItem>
+                        <DropdownItem onClick={this.dropUserText}>Drop user</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem>Show stats</DropdownItem>
-                        <DropdownItem>Show diagnostics</DropdownItem>
+                        <DropdownItem onClick={this.showStatsText}>Show stats</DropdownItem>
+                        <DropdownItem onClick={this.showDiagnosticsText}>Show diagnostics</DropdownItem>
                       </DropdownMenu>
                     </Dropdown>
                   </div>
@@ -96,7 +93,7 @@ class Main extends Component {
     );
   }
 
-  _onQueryChange(e) {
+  _onQueryChange = (e) => {
     this.setState({
       queryText: e.target.value
     });
@@ -107,6 +104,7 @@ class Main extends Component {
     this.setState({
       queryText: "SHOW DATABASES"
     });
+    this.focusQueryInput();
   };
 
   //  Create database
@@ -114,6 +112,7 @@ class Main extends Component {
     this.setState({
       queryText: `CREATE DATABASE "db_name"`
     });
+    this.focusQueryInput();
   };
 
   //  Drop database
@@ -121,6 +120,7 @@ class Main extends Component {
     this.setState({
       queryText: `DROP DATABASE "db_name"`
     });
+    this.focusQueryInput();
   };
 
   //  Show measurements
@@ -128,6 +128,7 @@ class Main extends Component {
     this.setState({
       queryText: "SHOW MEASUREMENTS"
     });
+    this.focusQueryInput();
   };
 
   //  Show tag keys
@@ -135,6 +136,7 @@ class Main extends Component {
     this.setState({
       queryText: `SHOW TAG KEYS FROM "measurement_name"`
     });
+    this.focusQueryInput();
   };
 
   //  Show tag values
@@ -142,6 +144,103 @@ class Main extends Component {
     this.setState({
       queryText: `SHOW TAG VALUES FROM "measurement_name" WITH KEY = "tag_key"`
     });
+    this.focusQueryInput();
+  };
+
+  //  Show retention policies
+  showRetentionPolicyText = () => {
+    this.setState({
+      queryText: `SHOW RETENTION POLICIES ON "db_name"`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Create retention policy
+  createRetentionPolicyText = () => {
+    this.setState({
+      queryText: `CREATE RETENTION POLICY "rp_name" ON "db_name" DURATION 30d REPLICATION 1 DEFAULT`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Drop retention policy
+  dropRetentionPolicyText = () => {
+    this.setState({
+      queryText: `DROP RETENTION POLICY "rp_name" ON "db_name"`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Show continuous queries
+  showContinuousQueryText = () => {
+    this.setState({
+      queryText: `SHOW CONTINUOUS QUERIES`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Create continuous query
+  createContinuousQueryText = () => {
+    this.setState({
+      queryText: `CREATE CONTINUOUS QUERY "cq_name" ON "db_name" BEGIN SELECT min("field") INTO "target_measurement" FROM "current_measurement" GROUP BY time(30m) END`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Drop continuous query
+  dropContinuousQueryText = () => {
+    this.setState({
+      queryText: `DROP CONTINUOUS QUERY "cq_name" ON "db_name"`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Show users
+  showUserText = () => {
+    this.setState({
+      queryText: `SHOW USERS`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Create user
+  createUserText = () => {
+    this.setState({
+      queryText: `CREATE USER "username" WITH PASSWORD 'password'`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Create admin user
+  createAdminUserText = () => {
+    this.setState({
+      queryText: `CREATE USER "username" WITH PASSWORD 'password' WITH ALL PRIVILEGES`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Drop user
+  dropUserText = () => {
+    this.setState({
+      queryText: `DROP USER "username"`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Show stats
+  showStatsText = () => {
+    this.setState({
+      queryText: `SHOW STATS`
+    });
+    this.focusQueryInput();
+  };
+
+  //  Show diagnostics
+  showDiagnosticsText = () => {
+    this.setState({
+      queryText: `SHOW DIAGNOSTICS`
+    });
+    this.focusQueryInput();
   };
 
   _onQuerySubmit= (e) => {
@@ -150,6 +249,11 @@ class Main extends Component {
 
     InfluxAPI.getQueryResults("telegraf", this.state.queryText);
   }
+
+  //  Focus input on the query textbox
+  focusQueryInput = () => {
+    this.queryInput.focus();
+  };
 }
 
 export default Main;
