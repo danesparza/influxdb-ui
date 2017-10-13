fdd7aee708aea7926f7342cf9c30ff1c335f466c
Dan Esparza <esparza.dan@gmail.com>
2017-10-13 13:37:48 -0400

    Update settings layout and styles

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -29,31 +29,24 @@ class Settings extends Component {
               <p className="lead text-muted settings-header">Servers</p>
               <div className="rounded settings-group">
 
-                <small>This is a list of InfluxDB servers you can connect to.  Remove any servers you don't recognize.</small>
-
-                <div className="list-group">
-                  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                      <h5 className="mb-1">Dev</h5>
-                      <button className="btn btn-outline-danger btn-sm">Delete</button>
-                    </div>
-                    <p className="mb-1">http://chile.lan:8086</p>
-                  </a>
-                  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                      <h5 className="mb-1">Test</h5>
-                      <button className="btn btn-outline-danger btn-sm">Delete</button>
-                    </div>
-                    <p className="mb-1">http://testserver:8086</p>
-                  </a>
-                  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                      <h5 className="mb-1">Prod</h5>
-                      <button className="btn btn-outline-danger btn-sm">Delete</button>
-                    </div>
-                    <p className="mb-1">http://prodserver:8086</p>
-                  </a>
+                <div className="settings-explanation">
+                  This is a list of InfluxDB servers you can connect to. 
                 </div>
+
+                <ul className="list-group">
+                  <li className="d-flex list-group-item justify-content-start flex-wrap">
+                    <div className="p-2"><h5>Dev</h5>http://chile.lan:8086 <span className="badge badge-success">Default</span></div>
+                    <div className="ml-auto p-2"><button className="btn btn-outline-danger btn-sm">Delete</button></div>
+                  </li>
+                  <li className="d-flex list-group-item justify-content-start flex-wrap">
+                  <div className="p-2"><h5>Test</h5>http://testserver:8086</div>
+                    <div className="ml-auto p-2"><button className="btn btn-outline-danger btn-sm">Delete</button></div>
+                  </li>
+                  <li className="d-flex list-group-item justify-content-start flex-wrap">
+                    <div className="p-2"><h5>Prod</h5>http://prodserver:8086</div>
+                    <div className="ml-auto p-2"><button className="btn btn-outline-danger btn-sm">Delete</button></div>
+                  </li>
+                </ul>
 
               </div>
 
