64aaa2a7dda827f08817aef596c51e4d0b198d95
Dan Esparza <esparza.dan@gmail.com>
2017-10-30 10:52:44 -0400

    Update jest test setup and CI build process

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,5 @@
 // TODO: Remove this `raf` polyfill once the below issue is sorted
 // https://github.com/facebookincubator/create-react-app/issues/3199#issuecomment-332842582
-import raf from './tempPolyfills'
+global.requestAnimationFrame = (cb) => {
+    setTimeout(cb, 0)
+}
