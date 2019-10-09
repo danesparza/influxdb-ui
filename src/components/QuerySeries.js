f1cad000f1ea23595d72d2e1a3ad30d6d3b96939
Dan Esparza <esparza.dan@gmail.com>
2019-10-09 15:28:25 -0400

    Start updating query result styles

--- src/components/QuerySeries.js
+++ src/components/QuerySeries.js
@@ -1,12 +1,28 @@
 //  React
 import React, { Component } from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 //  Components
 import QuerySeriesDataRow from './QuerySeriesDataRow';
+
+const styles = theme => ({
+    table: {
+      minWidth: 650,
+    },
+});
 
 class QuerySeries extends Component {  
 
   render() {
+
+    const { classes} = this.props;
+
     let seriesName = this.props.series.name;
     
     //  Set datarows if we have them:
@@ -18,25 +34,25 @@ class QuerySeries extends Component {
     //  Display series information:
     return (
         <div>
-            <h1 className="display-4">{this.props.series.name}</h1>
-            <table className="table table-striped table-sm">
-                <thead className="thead-inverse">
-                    <tr>
+            <h3>{this.props.series.name}</h3>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
                         {this.props.series.columns.map(function(col, index) {
-                            return <th key={index}>{col}</th>;
-                        })}
-                    </tr>
-                </thead>
-                <tbody>
-                {datarows.map(function(datarow, index) {
-                    return <QuerySeriesDataRow key={seriesName + "-" + index} datarow={datarow} />;
-                })}
-                </tbody>
-            </table>
+                                return <TableCell key={index}>{col}</TableCell>;
+                        })}                        
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {datarows.map(function(datarow, index) {
+                        return <QuerySeriesDataRow key={seriesName + "-" + index} datarow={datarow} />;
+                    })}
+                </TableBody>
+            </Table>            
         </div>
     );
   }
 
 }
 
-export default QuerySeries;
+export default withStyles(styles)(QuerySeries);
--- src/components/QuerySeriesDataRow.js
+++ src/components/QuerySeriesDataRow.js
@@ -1,16 +1,19 @@
 //  React
 import React, { Component } from 'react';
 
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 class QuerySeriesDataRow extends Component {  
 
   render() {
     //  Display data row:
     return (
-        <tr>
-            {this.props.datarow.map(function(item, index) {
-                return <td key={index}>{item}</td>;
-            })}
-        </tr>
+      <TableRow>
+          {this.props.datarow.map(function(item, index) {
+              return <TableCell key={index}>{item}</TableCell>;
+          })}                                  
+      </TableRow>        
     );
   }
 
