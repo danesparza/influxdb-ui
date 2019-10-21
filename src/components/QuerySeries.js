688dc626ed8f981fad53b101533096cc54a23b18
Dan Esparza <esparza.dan@gmail.com>
2019-10-21 15:34:37 -0400

    Remove sample data

--- src/components/QuerySeries.js
+++ src/components/QuerySeries.js
@@ -6,6 +6,8 @@ import { withStyles } from '@material-ui/core/styles';
 //  Components
 import MuiVirtualizedTable from './MuiVirtualizedTable';
 
+//  Styles
+
 const styles = theme => ({
     table: {
        minWidth: 650,
@@ -17,29 +19,9 @@ const styles = theme => ({
     }
 });
 
-const sample = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
-  ];
-  
-  function createData(id, dessert, calories, fat, carbs, protein) {
-    return { id, dessert, calories, fat, carbs, protein };
-  }
-    
-
 class QuerySeries extends Component {  
 
   render() {
-
-    const rows = [];
-  
-    for (let i = 0; i < 200; i += 1) {
-        const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-        rows.push(createData(i, ...randomSelection));
-    }
 
     const { classes} = this.props;
 
